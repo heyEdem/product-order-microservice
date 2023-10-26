f49cc6a2de09c2d0b16bd51db0bce9a97c0267df
heyEdem <akatoaugustine@gmail.com>
2023-10-26 23:58:05 +0000

    Edit the Inventory repo to implement findbyskuCodeIn

--- inventory-service/src/main/java/com/edem/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/edem/inventoryservice/service/InventoryService.java
@@ -6,14 +6,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class InventoryService {
     private final InventoryRepository repository;
     @Transactional
-    public boolean isInStock(String skuCode){
+    public boolean isInStock(List<String> skuCode){
          log.info("Product {} is in stock", skuCode);
-         return repository.findBySkuCode(skuCode).isPresent();
+         return repository.findBySkuCodeIn(skuCode).isPresent();
     }
 }
