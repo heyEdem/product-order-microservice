f21b2aa98428a28c7cc102de35ed30a823e7ce3b
heyEdem <akatoaugustine@gmail.com>
2023-10-27 22:36:41 +0000

    skuCodes variable to collect all skuCodes into a list in the OrderService class.

--- inventory-service/src/main/java/com/edem/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/edem/inventoryservice/service/InventoryService.java
@@ -17,12 +17,15 @@ public class InventoryService {
     @Transactional
     public List<InventoryResponse> isInStock(List<String> skuCode){
          log.info("Product {} is in stock", skuCode);
+
+         //find all items by the list of skuCodes given
          return repository.findBySkuCodeIn(skuCode).stream()
+                 // map the returned objects to a list of inventory responses
                  .map(inventory ->
                      InventoryResponse.builder()
                              .skuCode(inventory.getSkuCode())
                              .isInStock(inventory.getQuantity() > 0)
                              .build()
-                 ).toList().equals();
+                 ).toList();
     }
 }
--- order-service/src/main/java/com/edem/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/edem/orderservice/service/OrderService.java
@@ -32,6 +32,12 @@ public class OrderService {
                 .toList();
         order.setOrderLineItemsList(orderLineItemsList); //set the order items in that order
 
+        //Collect all skuCodes of items in an orderItemList into a list
+        // to be used by findBySkuCodeIn in the inventory service
+
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                                .map(OrderLineItems::getSkuCode).toList();
+
         //Check with the inventory service to see if the product is in stock
        Boolean result = webClient.get()
                 .uri("http://localhost:8082/api/inventory")
